a27d76bd54c1256d2adc050b38273552135b0af9
unknown <eviltwin@gmail.comgit config --global user name Konradg$ git config --global user.email eviltwin@gmail.comdsd>
2022-04-19 19:54:06 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String month = "Kwiecien";
+        int year = 2022;
+        long day = 19;
+        System.out.println("miesiac: " + month);
+        System.out.println("dzien: " + day);
+        System.out.println("rok: " + year);
+    }
+}
